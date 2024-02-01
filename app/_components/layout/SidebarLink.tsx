e651060bbcbdc90abaf71d34880f209242b50685
dongggggae <wjdqls149@naver.com>
2024-02-01 23:58:48 +0900

    [DS-76]Sidebar 기능 추가

--- app/_components/layout/SidebarLink.tsx
+++ app/_components/layout/SidebarLink.tsx
@@ -1,25 +1,53 @@
-// components/SidebarLink.tsx
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import Link from 'next/link';
 
-const SidebarLink: React.FC<{ link: Links }> = ({ link }) => (
-  <li className="nav__group show">
-    <Link href={link.href} className="nav__link">
-      {link.name}
-    </Link>
-    <ul className="nav__group-items">
-      <li className="nav__item"></li>
-      {link.children && (
-        <li className="nav__item">
-          {link.children.map((child: Links) => (
-            <Link key={child.name} href={child.href} className="nav__link">
-              {child.name}
-            </Link>
-          ))}
-        </li>
-      )}
-    </ul>
-  </li>
-);
+interface SidebarLinkProps {
+  link: Links;
+  menuOpen: boolean;
+  onMenuClick: (event: React.MouseEvent) => void;
+}
+
+const SidebarLink: React.FC<SidebarLinkProps> = ({ link, menuOpen, onMenuClick }) => {
+  const [activeLink, setActiveLink] = useState<string | null>(null);
+
+  const handleActiveLink = (linkName: string) => {
+    setActiveLink(linkName);
+  };
+
+  const handleGroupClick = (event: React.MouseEvent) => {
+    setActiveLink(null);
+    onMenuClick(event);
+  };
+
+  return (
+    <li className={`nav__group ${menuOpen ? 'show' : ''}`}>
+      <div className="menu">
+        <button className="menu-toggle__button" onClick={onMenuClick}></button>
+        <Link href={link.href} className={`nav__link`} onClick={handleGroupClick}>
+          {link.name}
+        </Link>
+      </div>
+
+      <ul className="nav__group-items">
+        <li className="nav__item"></li>
+        {link.children && (
+          <li className="nav__item">
+            {link.children.map((child: Links) => (
+              <Link
+                key={child.name}
+                href={child.href}
+                className={`nav__link ${activeLink === child.name ? 'active' : ''}`}
+                onClick={() => handleActiveLink(child.name)}
+              >
+                {child.name}
+              </Link>
+            ))}
+          </li>
+        )}
+      </ul>
+    </li>
+  );
+};
 
 export default SidebarLink;
