3ab866fa1b716f3972049eb2448a7acc9ef1a492
dongggggae <wjdqls149@naver.com>
2024-02-01 23:58:48 +0900

    [DS-76]Sidebar 기능 추가

--- app/_components/layout/SideBar.tsx
+++ app/_components/layout/SideBar.tsx
@@ -1,13 +1,26 @@
+'use client';
+import React, { useState } from 'react';
 import navLinks from '../../_data/nav';
 import SidebarLink from './SidebarLink';
 
 const Sidebar: React.FC = () => {
+  const [menuOpen, setMenuOpen] = useState<string | null>(null);
+
+  const toggleMenu = (menuName: string) => {
+    setMenuOpen((prevMenuOpen) => (prevMenuOpen === menuName ? null : menuName));
+  };
+
   return (
     <aside className="sidebar">
       <div className="navigation">
         <ul className="sidebar__nav">
           {navLinks.map((link) => (
-            <SidebarLink key={link.name} link={link} />
+            <SidebarLink
+              key={link.name}
+              link={link}
+              menuOpen={menuOpen === link.name}
+              onMenuClick={() => toggleMenu(link.name)}
+            />
           ))}
         </ul>
       </div>
