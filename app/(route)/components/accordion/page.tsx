90bb4bb18c94a542c87254c4f9b6834103b2243e
dongggggae <wjdqls149@naver.com>
2024-02-10 21:24:07 +0900

    뱃지 컴포넌트 삭제

--- app/(route)/components/accordion/page.tsx
+++ app/(route)/components/accordion/page.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
-import Card from '../../../_components/hds/card/Card';
 
 const AccorionPage: React.FC = () => {
   const route = '/components/badge';
 
-  return (
-    <div className="docs">
-      Accorion Page
-      <Card to={route}>
-        <Card.Img imageUrl={'/images/thumbnail.svg'} />
-        <Card.Title title="가나다라마바사" />
-        <Card.Text text="asdasdas" />
-      </Card>
-    </div>
-  );
+  return <div className="docs">Accorion Page</div>;
 };
 
 export default AccorionPage;
