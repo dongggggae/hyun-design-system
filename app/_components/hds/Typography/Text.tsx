d62b8cec0c3a1e80bf82e67e37cd0fedd98f6646
dongggggae <wjdqls149@naver.com>
2024-02-15 23:37:54 +0900

    [DS-80]Component - Text 추가

---
+++ app/_components/hds/Typography/Text.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import classNames from '@utils/classNames';
+
+interface TextProps {
+  children: React.ReactNode;
+  tag?: 'span' | 'strong' | 'p';
+  size?: 'lg' | 'md' | 'sm' | 'xs' | 'xxs';
+  color?: 'primary' | 'secondary' | 'tertiary' | 'information' | 'positive' | 'caution' | 'negative';
+  weight?: 'light' | 'regular' | 'medium' | 'semibold' | 'bold';
+}
+
+const Text: React.FC<TextProps> = ({ children, tag, size, color, weight }) => {
+  const textTag = tag || 'p';
+
+  return React.createElement(
+    textTag,
+    { className: classNames(size && `text--${size}`, color && `fc--${color}`, weight && `text--${size}-${weight}`) },
+    children,
+  );
+};
+
+Text.defaultProps = {
+  tag: 'p',
+  size: 'md',
+  color: 'secondary',
+};
+
+Text.displayName = 'Text';
+
+export default Text;
