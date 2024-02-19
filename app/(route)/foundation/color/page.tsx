c4de78dd65b8b3aec0f980873b3263a230d46e46
dongggggae <wjdqls149@naver.com>
2024-02-19 22:01:33 +0900

    [DS-82]Docs - Foundation, Heading, Text, Color 페이지 추가

--- app/(route)/foundation/color/page.tsx
+++ app/(route)/foundation/color/page.tsx
@@ -1,7 +1,61 @@
 import React from 'react';
 
+import Heading from '@components/Typography/Heading';
+import Text from '@components/Typography/Text';
+import colors from '@themes/tokens/colors';
+
 const ColorPage: React.FC = () => {
-  return <div className="docs">ColorPage</div>;
+  const colorKeys = Object.keys(colors);
+
+  console.log(colorKeys);
+  return (
+    <div className="docs">
+      <div className="docs-color">
+        {/* GrayColor */}
+        <div className="component-content">
+          <Heading level="h2" size={2} text="1. Gray" />
+          <Text>다음은 Gray Scale을 나타냅니다.</Text>
+          <div className="color__wrap">
+            {colorKeys.slice(0, 17).map((color, index) => (
+              <div className="color__unit" key={index}>
+                <div className="color__box" style={{ backgroundColor: colors[color] }}></div>
+                <Text>{color}</Text>
+              </div>
+            ))}
+          </div>
+        </div>
+        {/* //GrayColor */}
+        {/* Green & Blue & Navy Color */}
+        <div className="component-content">
+          <Heading level="h2" size={2} text="2. Green & Blue & Navy" />
+          <Text>다음은 Green & Blue & Navy Scale을 나타냅니다.</Text>
+          <div className="color__wrap">
+            {colorKeys.slice(27, 43).map((color, index) => (
+              <div className="color__unit" key={index}>
+                <div className="color__box" style={{ backgroundColor: colors[color] }}></div>
+                <Text>{color}</Text>
+              </div>
+            ))}
+          </div>
+        </div>
+        {/* //Green & Blue & Navy Color */}
+        {/* Red & Ember & Navy Color */}
+        <div className="component-content">
+          <Heading level="h2" size={2} text="3. Red & Ember" />
+          <Text>다음은 Red & Ember Scale을 나타냅니다.</Text>
+          <div className="color__wrap">
+            {colorKeys.slice(17, 27).map((color, index) => (
+              <div className="color__unit" key={index}>
+                <div className="color__box" style={{ backgroundColor: colors[color] }}></div>
+                <Text>{color}</Text>
+              </div>
+            ))}
+          </div>
+        </div>
+        {/* //Red & Ember & Navy Color */}
+      </div>
+    </div>
+  );
 };
 
 export default ColorPage;
--- app/(route)/foundation/heading/page.tsx
+++ app/(route)/foundation/heading/page.tsx
@@ -1,7 +1,154 @@
 import React from 'react';
+import dynamic from 'next/dynamic';
+
+import Heading from '@components/Typography/Heading';
+import Text from '@components/Typography/Text';
+
+const CodeContainer = dynamic(() => import('@docs/CodeContainer'));
 
 const HeadingPage: React.FC = () => {
-  return <div className="docs">HeadingPage</div>;
+  return (
+    <div className="docs">
+      <div className="docs-heading">
+        {/* Overview */}
+        <div className="component-content">
+          <Heading level="h2" size={2} text="1. 개요" />
+          <Text>Heading 컴포넌트를 통해 헤더 요소를 생성합니다.</Text>
+          <div className="preview">
+            <Heading level="h1" text="Default Title" />
+            <Heading level="h3" size={3} text="H3 Title" />
+          </div>
+          <CodeContainer codeString={HeadingCode.overview} />
+        </div>
+        {/* //Overview */}
+        {/* Level */}
+        <div className="component-content">
+          <Heading level="h2" size={2} text="2. Level" />
+          <Text>Level 프로퍼티를 통해 렌더링 되는 태그를 변경합니다.</Text>
+          <div className="preview">
+            <Heading level="h1" size={1} text="H1 Title" />
+            <Heading level="h2" size={2} text="H2 Title" />
+            <Heading level="h3" size={3} text="H3 Title" />
+            <Heading level="h4" size={4} text="H4 Title" />
+            <Heading level="h5" size={5} text="H5 Title" />
+            <Heading level="h6" size={6} text="H6 Title" />
+          </div>
+          <CodeContainer codeString={HeadingCode.level} />
+        </div>
+        {/* //Level */}
+        {/* Color */}
+        <div className="component-content">
+          <Heading level="h2" size={2} text="3. Color" />
+          <Text>
+            Color 프로퍼티를 통해 헤더 컬러를 지정하며, 기본 값은 &#39;#111111&#39; 입니다 다른 컬러를 적용하고 싶은
+            경우에 사용합니다.
+            <br />
+            아래는 Color 프로퍼티를 통해 컬러를 적용한 예시입니다.
+          </Text>
+          <div className="preview">
+            <Heading level="h3" size={3} color="primary" text="Default Color" />
+            <Heading level="h3" size={3} color="secondary" text="Secondary Color" />
+            <Heading level="h3" size={3} color="tertiary" text="Tertiary Color" />
+            <Heading level="h3" size={3} color="information" text="Information Color" />
+            <Heading level="h3" size={3} color="positive" text="Positive Color" />
+            <Heading level="h3" size={3} color="caution" text="Caution Color" />
+            <Heading level="h3" size={3} color="negative" text="Negative Color" />
+          </div>
+          <CodeContainer codeString={HeadingCode.color} />
+        </div>
+        {/* //Color */}
+        {/* Size */}
+        <div className="component-content">
+          <Heading level="h2" size={2} text="4. Size" />
+          <Text>
+            Size 프로퍼티를 통해 헤더 사이즈를 지정하며, 해당 태그에 다른 크기를 적용하고 싶은 경우에 사용합니다.
+            <br />
+            아래는 h1 태그로 Size 프로퍼티를 통해 사이즈를 적용한 예시입니다.
+          </Text>
+          <div className="preview">
+            <Heading level="h1" size={1} text="H1 Title" />
+            <Heading level="h1" size={2} text="H2 Title" />
+            <Heading level="h1" size={3} text="H3 Title" />
+            <Heading level="h1" size={4} text="H4 Title" />
+            <Heading level="h1" size={5} text="H5 Title" />
+            <Heading level="h1" size={6} text="H6 Title" />
+          </div>
+          <CodeContainer codeString={HeadingCode.level} />
+        </div>
+        {/* //Size */}
+      </div>
+    </div>
+  );
+};
+
+const HeadingCode = {
+  overview: [
+    `<!-- Default Heading -->`,
+    `<Heading level="h1" text="Default Title" />`,
+    ``,
+    `<!-- H3 Heading -->`,
+    `<Heading level="h3" size={3} text="H3 Title" />`,
+  ].join('\n'),
+  level: [
+    `<!-- H1 Heading -->`,
+    `<Heading level="h1" size={1} text="H1 Title" />`,
+    ``,
+    `<!-- H2 Heading -->`,
+    `<Heading level="h2" size={2} text="H2 Title" />`,
+    ``,
+    `<!-- H3 Heading -->`,
+    `<Heading level="h3" size={3} text="H3 Title" />`,
+    ``,
+    `<!-- H4 Heading -->`,
+    `<Heading level="h4" size={4} text="H4 Title" />`,
+    ``,
+    `<!-- H5 Heading -->`,
+    `<Heading level="h5" size={5} text="H5 Title" />`,
+    ``,
+    `<!-- H6 Heading -->`,
+    `<Heading level="h6" size={6} text="H6 Title" />`,
+  ].join('\n'),
+  color: [
+    `<!-- Heading Default Color -->`,
+    `<Heading level="h3" size='3' text="Default Color" />`,
+    ``,
+    `<!-- Heading Secondary Color -->`,
+    `<Heading level="h3" size='3' color="secondary" text="Secondary Color" />`,
+    ``,
+    `<!-- Heading Tertiary Color -->`,
+    `<Heading level="h3" size='3' color="tertiary" text="Tertiary Color" />`,
+    ``,
+    `<!-- Heading Information Color -->`,
+    `<Heading level="h3" size='3' color="information" text="Information Color" />`,
+    ``,
+    `<!-- Heading Positive Color -->`,
+    `<Heading level="h3" size='3' color="positive" text="Positive Color" />`,
+    ``,
+    `<!-- Heading Caution Color -->`,
+    `<Heading level="h3" size='3' color="caution" text="Caution Color" />`,
+    ``,
+    `<!-- Heading Negative Color -->`,
+    `<Heading level="h3" size='3' color="negative" text="Negative Color" />`,
+  ].join('\n'),
+  size: [
+    `<!-- H1 Heading -->`,
+    `<Heading level="h1" size='1' text="H1 Size" />`,
+    ``,
+    `<!-- H2 Heading -->`,
+    `<Heading level="h1" size='2' text="H2 Size" />`,
+    ``,
+    `<!-- H3 Heading -->`,
+    `<Heading level="h1" size='3' text="H3 Size" />`,
+    ``,
+    `<!-- H4 Heading -->`,
+    `<Heading level="h1" size='4' text="H4 Size" />`,
+    ``,
+    `<!-- H5 Heading -->`,
+    `<Heading level="h1" size='5' text="H5 Size" />`,
+    ``,
+    `<!-- H6 Heading -->`,
+    `<Heading level="h1" size='6' text="H6 Size" />`,
+  ].join('\n'),
 };
 
 export default HeadingPage;
