4738e73a8ff80dd27a98cb5c7b1345c65cf4fe41
김동현 <doyn0822@hiconsy.com>
2024-02-28 21:50:03 +0900

    [feature/DS-81]Docs - Badge 추가

--- app/(route)/components/badge/page.tsx
+++ app/(route)/components/badge/page.tsx
@@ -1,7 +1,112 @@
 import React from 'react';
+import dynamic from 'next/dynamic';
+
+import Heading from '@components/Typography/Heading';
+import Text from '@components/Typography/Text';
+import Stack from '@components/Stack/Stack';
+import Badge from '@components/Badge/Badge';
+
+const CodeContainer = dynamic(() => import('@docs/CodeContainer'));
 
 const BadgePage = () => {
-  return <div className="docs">Badge Page</div>;
+  return (
+    <div className="docs">
+      <div className="docs-badge">
+        {/* Overview */}
+        <div className="component-content">
+          <Heading level="h2" size={2} text="1. 개요" />
+          <Text>Badge 컴포넌트를 통해 뱃지 요소를 생성합니다.</Text>
+          <div className="preview">
+            <Badge text="Default Badge" />
+          </div>
+          <CodeContainer codeString={badgeCode.overview} />
+        </div>
+        {/* Overview */}
+        {/* State */}
+        <div className="component-content">
+          <Heading level="h2" size={2} text="2. State" />
+          <Text>뱃지 스타일은 State 프로퍼티를 통해 변경합니다. 기본 값은 &#39;information&#39; 입니다.</Text>
+          <div className="preview">
+            <Badge text="Default Badge" />
+            <Badge text="Warnning Badge" state="warnning" />
+            <Badge text="Error Badge" state="error" />
+            <Badge text="Information Badge" state="information" />
+          </div>
+          <CodeContainer codeString={badgeCode.state} />
+        </div>
+        {/* State */}
+        {/* Outline */}
+        <div className="component-content">
+          <Heading level="h2" size={2} text="3. Outline" />
+          <Text>뱃지 Outline 프로퍼티를 통해 뱃지의 Outline을 설정합니다. 기본 값은 &#39;false&#39; 입니다.</Text>
+          <div className="preview">
+            <Badge text="Default Badge" outline />
+            <Badge text="Warnning Badge" state="warnning" outline />
+            <Badge text="Error Badge" state="error" outline />
+            <Badge text="Information Badge" state="information" outline />
+          </div>
+          <CodeContainer codeString={badgeCode.outline} />
+        </div>
+        {/* Outline */}
+        {/* Outline */}
+        <div className="component-content">
+          <Heading level="h2" size={2} text="4. Size" />
+          <Text>Size 프로퍼티를 통해 뱃지 사이즈를 지정하며, 기본 값은 &#39;sm&#39; 입니다.</Text>
+          <div className="preview">
+            <Badge text="Default Badge" />
+            <Badge text="Default Badge" size="md" />
+            <Badge text="Error Badge" state="error" />
+            <Badge text="Error Badge" state="error" size="md" />
+          </div>
+          <CodeContainer codeString={badgeCode.size} />
+        </div>
+        {/* Outline */}
+      </div>
+    </div>
+  );
+};
+
+const badgeCode = {
+  overview: [`<!-- Default Badge -->`, '<Badge text="Default Badge" />'].join('\n'),
+  state: [
+    `<!-- Default Badge -->`,
+    `<Badge text="Default Badge" />`,
+    ``,
+    `<!-- Warnning Badge -->`,
+    `<Badge text="Warnning Badge" state="warnning" />`,
+    ``,
+    `<!-- Error Badge -->`,
+    `<Badge text="Error Badge" state="error" />`,
+    ``,
+    `<!-- Information Badge -->`,
+    `<Badge text="Information Badge" state="information" />`,
+  ].join(`\n`),
+  outline: [
+    `<!-- Default Badge -->`,
+    `<Badge text="Default Outline Badge" outline />`,
+    ``,
+    `<!-- Warnning Badge -->`,
+    `<Badge text="Warnning Outline Badge" state="warnning" outline />`,
+    ``,
+    `<!-- Error Badge -->`,
+    `<Badge text="Error Outline Badge" state="error" outline />`,
+    ``,
+    `<!-- Information Badge -->`,
+    `<Badge text="Information Outline Badge" state="information" outline />`,
+  ].join(`\n`),
+  size: [
+    `<!-- Small Size Badge -->`,
+    `<Badge text="Small Size Badge" />`,
+    ``,
+    `<!-- Medium Size Badge -->`,
+    `<Badge text="Medium Size Badge" size="md" />`,
+    ``,
+    `<!-- Small Size Badge -->`,
+    `<Badge text="Small Size Badge" state="error" />`,
+    ``,
+    `<!-- Medium Size Badge -->`,
+    `<Badge text="Medium Size Badge" state="error" size="md" />`,
+  ].join(`\n`),
 };
 
 export default BadgePage;
