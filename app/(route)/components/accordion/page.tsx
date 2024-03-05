23cef15b6ea13c380181f6d20850c6e23ee4dd23
김동현 <doyn0822@hiconsy.com>
2024-03-05 14:42:05 +0900

    [feature/DS-86]Docs - Accordion 추가

--- app/(route)/components/accordion/page.tsx
+++ app/(route)/components/accordion/page.tsx
@@ -1,7 +1,184 @@
+'use client';
 import React from 'react';
+import dynamic from 'next/dynamic';
+
+import Heading from '@components/Typography/Heading';
+import Text from '@components/Typography/Text';
+import Accordion from '@components/Accordion/Accordion';
+
+const CodeContainer = dynamic(() => import('@docs/CodeContainer'));
 
 const AccorionPage = () => {
-  return <div className="docs">Accorion Page</div>;
+  return (
+    <div className="docs">
+      <div className="docs-accordion">
+        {/* Overview */}
+        <div className="component-content">
+          <Heading level="h2" size={2} text="1. 개요" />
+          <Text>Accordion 컴포넌트를 통해 아코디언 요소를 생성합니다.</Text>
+          <div className="preview fill">
+            <Accordion>
+              <Accordion.Item title="Accordion Title1">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas veniam quisquam quae officiis nemo,
+                  natus autem placeat optio at qui nobis quod, maxime in aspernatur sint sed eaqueitaque ea.
+                </p>
+              </Accordion.Item>
+              <Accordion.Item title="Accordion Title2">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas veniam quisquam quae officiis nemo,
+                  natus autem placeat optio at qui nobis quod, maxime in aspernatur sint sed eaqueitaque ea.
+                </p>
+              </Accordion.Item>
+              <Accordion.Item title="Accordion Title3">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas veniam quisquam quae officiis nemo,
+                  natus autem placeat optio at qui nobis quod, maxime in aspernatur sint sed eaqueitaque ea.
+                </p>
+              </Accordion.Item>
+            </Accordion>
+          </div>
+          <CodeContainer codeString={accordionCode.overview} />
+        </div>
+        {/* //Overview */}
+        <div className="component-content">
+          <Heading level="h2" size={2} text="2. Title" />
+          <Text>Title 프로퍼티를 통해 아코디언의 제목을 지정할 수 있습니다.</Text>
+          <div className="preview fill">
+            <Accordion>
+              <Accordion.Item title="Accordion Title1">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas veniam quisquam quae officiis nemo,
+                  natus autem placeat optio at qui nobis quod, maxime in aspernatur sint sed eaqueitaque ea.
+                </p>
+              </Accordion.Item>
+              <Accordion.Item title="Accordion Title2">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas veniam quisquam quae officiis nemo,
+                  natus autem placeat optio at qui nobis quod, maxime in aspernatur sint sed eaqueitaque ea.
+                </p>
+              </Accordion.Item>
+              <Accordion.Item title="Accordion Title3">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas veniam quisquam quae officiis nemo,
+                  natus autem placeat optio at qui nobis quod, maxime in aspernatur sint sed eaqueitaque ea.
+                </p>
+              </Accordion.Item>
+            </Accordion>
+          </div>
+          <CodeContainer codeString={accordionCode.title} />
+        </div>
+        <div className="component-content">
+          <Heading level="h2" size={2} text="3. isOpen" />
+          <Text>AccordionItem 컴포넌트의 isOpen 프로퍼티를 통해 해당 컨텐츠가 펼쳐진 채로 나타납니다.</Text>
+          <div className="preview fill">
+            <Accordion>
+              <Accordion.Item title="Accordion Title1" isOpen>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas veniam quisquam quae officiis nemo,
+                  natus autem placeat optio at qui nobis quod, maxime in aspernatur sint sed eaqueitaque ea.
+                </p>
+              </Accordion.Item>
+              <Accordion.Item title="Accordion Title2" isOpen>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas veniam quisquam quae officiis nemo,
+                  natus autem placeat optio at qui nobis quod, maxime in aspernatur sint sed eaqueitaque ea.
+                </p>
+              </Accordion.Item>
+              <Accordion.Item title="Accordion Title3" isOpen>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas veniam quisquam quae officiis nemo,
+                  natus autem placeat optio at qui nobis quod, maxime in aspernatur sint sed eaqueitaque ea.
+                </p>
+              </Accordion.Item>
+            </Accordion>
+          </div>
+          <CodeContainer codeString={accordionCode.isOpen} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const accordionCode = {
+  overview: [
+    `<!-- Default Accordion -->`,
+    ` <Accordion>`,
+    `   <Accordion.Item title="Accordion Title1">`,
+    `     <p>`,
+    `       Lorem ipsum dolor sit amet consectetur adipisicing elit. `,
+    `       Quas veniam quisquam quae officiis nemo, natus autem placeat optio at qui nobis quod, `,
+    `       maxime in aspernatur sint sed eaqueitaque ea.`,
+    `     </p>`,
+    `   </Accordion.Item>`,
+    `   <Accordion.Item title="Accordion Title2">`,
+    `     <p>`,
+    `       Lorem ipsum dolor sit amet consectetur adipisicing elit. `,
+    `       Quas veniam quisquam quae officiis nemo, natus autem placeat optio at qui nobis quod, `,
+    `       maxime in aspernatur sint sed eaqueitaque ea.`,
+    `     </p>`,
+    `   </Accordion.Item>`,
+    `   <Accordion.Item title="Accordion Title3">`,
+    `     <p>`,
+    `       Lorem ipsum dolor sit amet consectetur adipisicing elit. `,
+    `       Quas veniam quisquam quae officiis nemo, natus autem placeat optio at qui nobis quod, `,
+    `       maxime in aspernatur sint sed eaqueitaque ea.`,
+    `     </p>`,
+    `    </Accordion.Item>`,
+    `</Accordion>`,
+  ].join(`\n`),
+  title: [
+    `<!-- Default Accordion -->`,
+    ` <Accordion>`,
+    `   <Accordion.Item title="Accordion Title1">`,
+    `     <p>`,
+    `       Lorem ipsum dolor sit amet consectetur adipisicing elit. `,
+    `       Quas veniam quisquam quae officiis nemo, natus autem placeat optio at qui nobis quod, `,
+    `       maxime in aspernatur sint sed eaqueitaque ea.`,
+    `     </p>`,
+    `   </Accordion.Item>`,
+    `   <Accordion.Item title="Accordion Title2">`,
+    `     <p>`,
+    `       Lorem ipsum dolor sit amet consectetur adipisicing elit. `,
+    `       Quas veniam quisquam quae officiis nemo, natus autem placeat optio at qui nobis quod, `,
+    `       maxime in aspernatur sint sed eaqueitaque ea.`,
+    `     </p>`,
+    `   </Accordion.Item>`,
+    `   <Accordion.Item title="Accordion Title3">`,
+    `     <p>`,
+    `       Lorem ipsum dolor sit amet consectetur adipisicing elit. `,
+    `       Quas veniam quisquam quae officiis nemo, natus autem placeat optio at qui nobis quod, `,
+    `       maxime in aspernatur sint sed eaqueitaque ea.`,
+    `     </p>`,
+    `    </Accordion.Item>`,
+    `</Accordion>`,
+  ].join(`\n`),
+  isOpen: [
+    `<!-- Default Accordion -->`,
+    ` <Accordion>`,
+    `   <Accordion.Item title="Accordion Title1" isOpen >`,
+    `     <p>`,
+    `       Lorem ipsum dolor sit amet consectetur adipisicing elit. `,
+    `       Quas veniam quisquam quae officiis nemo, natus autem placeat optio at qui nobis quod, `,
+    `       maxime in aspernatur sint sed eaqueitaque ea.`,
+    `     </p>`,
+    `   </Accordion.Item>`,
+    `   <Accordion.Item title="Accordion Title2" isOpen >`,
+    `     <p>`,
+    `       Lorem ipsum dolor sit amet consectetur adipisicing elit. `,
+    `       Quas veniam quisquam quae officiis nemo, natus autem placeat optio at qui nobis quod, `,
+    `       maxime in aspernatur sint sed eaqueitaque ea.`,
+    `     </p>`,
+    `   </Accordion.Item>`,
+    `   <Accordion.Item title="Accordion Title3" isOpen >`,
+    `     <p>`,
+    `       Lorem ipsum dolor sit amet consectetur adipisicing elit. `,
+    `       Quas veniam quisquam quae officiis nemo, natus autem placeat optio at qui nobis quod, `,
+    `       maxime in aspernatur sint sed eaqueitaque ea.`,
+    `     </p>`,
+    `    </Accordion.Item>`,
+    `</Accordion>`,
+  ].join(`\n`),
 };
 
 export default AccorionPage;
--- app/_components/hds/Accordion/Accordion.tsx
+++ app/_components/hds/Accordion/Accordion.tsx
@@ -1,17 +1,22 @@
 'use client';
 import React from 'react';
+
 import classNames from '@utils/classNames';
+
+import AccordionItem from './AccordionItem';
 
 interface AccorionProps {
   children: React.ReactNode;
   className?: string | string[];
 }
 
-const Accorion: React.FC<AccorionProps> = ({ children, className }) => {
+const Accordion: React.FC<AccorionProps> = ({ children, className }) => {
   const PREFIX = 'accordion';
   return (
     <div className={classNames(PREFIX, Array.isArray(className) ? className.join(' ') : className)}>{children}</div>
   );
 };
 
-export default Accorion;
+export default Object.assign(Accordion, {
+  Item: AccordionItem,
+});
