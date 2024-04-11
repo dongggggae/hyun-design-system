0554fc391b29e8c102a59eabba030b4ef46a31a8
김동현 <doyn0822@hiconsy.com>
2024-04-11 16:52:52 +0900

    [DS-17]Component - Input 추가

--- app/_components/hds/Forms/FormsInput.tsx
+++ app/_components/hds/Forms/FormsInput.tsx
@@ -1,63 +1,35 @@
-'use client';
-// FormsInput.tsx
-import React, { useState } from 'react';
+import React, { forwardRef } from 'react';
 
 import classNames from '@utils/classNames';
 
-import FormsAddon from './FormsAddon';
-import useInput from './FormsContext';
-
-interface InputProps {
+interface InputProps extends React.HTMLProps<HTMLInputElement> {
   id: string;
   type: string;
   placeholder?: string;
   disabled?: boolean;
-  className?: string | string[];
+  className?: string | undefined;
 }
 
-const FormsInput: React.FC<InputProps> = ({ id, type, placeholder, disabled, className }) => {
-  const PREFIX = 'form__control';
+const FormsInput = forwardRef<HTMLInputElement, InputProps>(
+  ({ id, type, placeholder, disabled, className, ...props }, ref) => {
+    const PREFIX = 'form__control';
 
-  const { inputText, onChange } = useInput();
-  const [passwordVisible, setPasswordVisible] = useState(false);
+    return (
+      <div className="input-wrap">
+        <input
+          id={id}
+          ref={ref}
+          type={type}
+          disabled={disabled}
+          placeholder={placeholder}
+          className={classNames(PREFIX, Array.isArray(className) ? className.join(' ') : className)}
+          {...props}
+        />
+      </div>
+    );
+  },
+);
 
-  const togglePasswordVisibility = () => {
-    setPasswordVisible(!passwordVisible);
-  };
-
-  const clearInput = () => {
-    onChange({ target: { value: '' } } as React.ChangeEvent<HTMLInputElement>);
-  };
-
-  const renderAddon = () => {
-    switch (type) {
-      case 'text':
-        return inputText.length > 0 ? <FormsAddon field="clear" onClick={clearInput} /> : null;
-      case 'search':
-        return inputText.length > 0 ? <FormsAddon field="search" /> : null;
-      case 'password':
-        return <FormsAddon field={passwordVisible ? 'visible' : 'invisible'} onClick={togglePasswordVisibility} />;
-      default:
-        return null;
-    }
-  };
-
-  const inputType = type === 'password' && passwordVisible ? 'text' : type;
-
-  return (
-    <div className="input-wrap">
-      <input
-        id={id}
-        type={inputType}
-        disabled={disabled}
-        onChange={onChange}
-        placeholder={placeholder}
-        value={inputText}
-        className={classNames(PREFIX, Array.isArray(className) ? className.join(' ') : className)}
-      />
-      {renderAddon()}
-    </div>
-  );
-};
+FormsInput.displayName = 'FormsInput';
 
 export default FormsInput;
---
+++ app/_components/hds/Forms/Temp.tsx
@@ -0,0 +1,65 @@
+import React, { useState, useRef, forwardRef } from 'react';
+
+import classNames from '@utils/classNames';
+
+import FormsAddon from './FormsAddon';
+import useInput from './FormsContext';
+
+interface InputProps {
+  id: string;
+  type: string;
+  placeholder?: string;
+  disabled?: boolean;
+  className?: string | string[];
+}
+
+const Temp: React.ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { id, type, placeholder, disabled, className, ...props },
+  ref,
+) => {
+  const PREFIX = 'form__control';
+
+  const { inputText, onChange } = useInput();
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
+  const clearInput = () => {
+    onChange({ target: { value: '' } } as React.ChangeEvent<HTMLInputElement>);
+  };
+
+  const renderAddon = () => {
+    switch (type) {
+      case 'text':
+        return inputText.length > 0 ? <FormsAddon field="clear" onClick={clearInput} /> : null;
+      case 'search':
+        return inputText.length > 0 ? <FormsAddon field="search" /> : null;
+      case 'password':
+        return <FormsAddon field={passwordVisible ? 'visible' : 'invisible'} onClick={togglePasswordVisibility} />;
+      default:
+        return null;
+    }
+  };
+
+  const inputType = type === 'password' && passwordVisible ? 'text' : type;
+
+  return (
+    <div className="input-wrap">
+      <input
+        id={id}
+        type={inputType}
+        disabled={disabled}
+        onChange={onChange}
+        placeholder={placeholder}
+        value={inputText}
+        {...props}
+        className={classNames(PREFIX, Array.isArray(className) ? className.join(' ') : className)}
+      />
+      {renderAddon()}
+    </div>
+  );
+};
+
+export default forwardRef(Temp);
