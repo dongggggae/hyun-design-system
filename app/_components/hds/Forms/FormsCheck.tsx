1234db54c18da2e35b8babe6b395b4b444d0bcb4
김동현 <doyn0822@hiconsy.com>
2024-04-12 15:52:13 +0900

    [DS-18]Component - Checkbox Style 추가 및 수정

---
+++ app/_components/hds/Forms/FormsCheck.tsx
@@ -0,0 +1,33 @@
+import React, { forwardRef } from 'react';
+
+import classNames from '@utils/classNames';
+
+interface InputCheckProps extends React.HTMLProps<HTMLInputElement> {
+  id: string;
+  type?: string;
+  checkd?: boolean;
+  className?: string | undefined;
+}
+
+const FormsCheck = forwardRef<HTMLInputElement, InputCheckProps>(
+  ({ id, type = 'checkbox', checkd, className, ...props }, ref) => {
+    const PREFIX = 'form__check';
+
+    return (
+      <div className={classNames(PREFIX)}>
+        <input
+          id={id}
+          ref={ref}
+          type={type}
+          checked={checkd}
+          className={classNames(`${PREFIX}-input`, className)}
+          {...props}
+        />
+      </div>
+    );
+  },
+);
+
+FormsCheck.displayName = 'FormsCheck';
+
+export default FormsCheck;
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,7 @@
-'use client';
-import { DevTool } from '@hookform/devtools';
 import React from 'react';
-import { useForm } from 'react-hook-form';
-
-import FormsInput from '@components/Forms/FormsInput';
-
-import classNames from '@utils/classNames';
 
 const Home = () => {
-  const { register, control, handleSubmit } = useForm({
-    mode: 'onChange',
-  });
-
-  return (
-    <div className="docs">
-      <form onSubmit={handleSubmit((data) => console.log(data))}>
-        <FormsInput id="test" type="text" placeholder="테스트" {...register('test')} />
-        <button type="submit">제출</button>
-      </form>
-      {/* <DevTool control={control} /> */}
-    </div>
-  );
+  return <div className="docs">HomePage</div>;
 };
 
 export default Home;
