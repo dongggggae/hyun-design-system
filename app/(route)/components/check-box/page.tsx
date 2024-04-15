00478b3d46d12879f0b6a19ca89f303288836a80
김동현 <doyn0822@hiconsy.com>
2024-04-15 12:33:23 +0900

    Component - Btn : HTMLAttributes 추가

--- app/(route)/components/check-box/page.tsx
+++ app/(route)/components/check-box/page.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
+import Btn from '@components/Btn/Btn';
 import FormsCheck from '@components/Forms/FormsCheck';
 
 const CheckboxPage = () => {
@@ -16,7 +17,7 @@ const CheckboxPage = () => {
         <FormsCheck id="check2" type="radio" value="aaa" {...register('check2', { required: 'check' })} />
         <FormsCheck id="check3" type="checkbox" {...register('check3', { required: 'check' })} />
         <FormsCheck id="check4" type="checkbox" {...register('check4', { required: 'check' })} />
-        <button type="submit">제출</button>
+        <Btn type="submit" text="제출" />
       </form>
     </div>
   );
--- app/(route)/components/text-field/page.tsx
+++ app/(route)/components/text-field/page.tsx
@@ -3,6 +3,7 @@ import { DevTool } from '@hookform/devtools';
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
+import Btn from '@components/Btn/Btn';
 import FormsInput from '@components/Forms/FormsInput';
 
 const TextFiledPage = () => {
@@ -15,7 +16,7 @@ const TextFiledPage = () => {
       TextFiledPage
       <form onSubmit={handleSubmit((data) => console.log(data))}>
         <FormsInput id="test" type="text" placeholder="테스트" {...register('test', { required: '우라라라' })} />
-        <button type="submit">제출</button>
+        <Btn type="submit" text="제출" />
       </form>
       {/* <DevTool control={control} /> */}
     </div>
