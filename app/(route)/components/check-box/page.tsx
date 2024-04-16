5c7bd15744d6dbd93b223f5fbcddbe513ec57ff8
김동현 <doyn0822@hiconsy.com>
2024-04-16 18:12:37 +0900

    작업 중

--- app/(route)/components/check-box/page.tsx
+++ app/(route)/components/check-box/page.tsx
@@ -1,18 +1,38 @@
 'use client';
-import React from 'react';
+import dynamic from 'next/dynamic';
+import React, { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 
-import Btn from '@components/Btn/Btn';
 import FormsCheck from '@components/Forms/FormsCheck';
+import Stack from '@components/Stack/Stack';
+import Heading from '@components/Typography/Heading';
+import Text from '@components/Typography/Text';
+
+const CodeContainer = dynamic(() => import('@docs/CodeContainer'));
 
 const CheckboxPage = () => {
-  const { register, control, handleSubmit } = useForm({
-    mode: 'onChange',
-  });
+  const { register, watch } = useForm();
+  const checkedValues = watch(['checkBox1', 'checkBox2']);
 
   return (
     <div className="docs">
-      <FormsCheck id="check2" type="radio" {...register('check1', { required: 'check' })} label="2번 checkBox" />
+      <div className="docs-checkBox">
+        <div className="component-content">
+          <Heading level="h2" size={2} text="1. 개요" />
+          <Text>FormsCheck 컴포넌트를 통해 체크박스 요소를 생성합니다.</Text>
+          <div className="preview">
+            <form>
+              <Stack direction="col" gap={4}>
+                <Stack gap={4}>
+                  <FormsCheck id="checkBox1" type="checkbox" label="checkBox1" {...register('checkBox1')} />
+                  <FormsCheck id="checkBox2" type="checkbox" label="checkBox2" {...register('checkBox2')} />
+                </Stack>
+              </Stack>
+            </form>
+          </div>
+        </div>
+      </div>
+      {/* <FormsCheck id="check2" type="radio" {...register('check1', { required: 'check' })} label="2번 checkBox" />
       <FormsCheck
         id="check1"
         reverse
@@ -33,7 +53,7 @@ const CheckboxPage = () => {
         type="checkbox"
         {...register('check4', { required: 'check' })}
         label="4번 checkBox"
-      />
+      /> */}
     </div>
   );
 };
