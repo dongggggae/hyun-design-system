36c1b15ec889ea6dc76a1a1d6befae0f7c969eaa
김동현 <doyn0822@hiconsy.com>
2024-04-29 12:37:43 +0900

    [DS-19]Component - Select 스타일 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 
+import Stack from '@components/Stack/Stack';
+
 const Home = () => {
   return (
     <div className="docs">
-      <select name="" id="" className="form__select">
-        <option value="aaa">aaaa</option>
-        <option value="aaa">aaaa</option>
-      </select>
+      <Stack direction="col">
+        <select name="" id="" className="form__select" disabled>
+          <option value="aaa">aaaa</option>
+          <option value="aaa">aaaa</option>
+        </select>
+        <select name="" id="" className="form__select">
+          <option value="aaa">aaaa</option>
+          <option value="aaa">aaaa</option>
+        </select>
+        <select name="" id="" className="form__select is-valid">
+          <option value="aaa">aaaa</option>
+          <option value="aaa">aaaa</option>
+        </select>
+        <select name="" id="" className="form__select is-invalid">
+          <option value="aaa">aaaa</option>
+          <option value="aaa">aaaa</option>
+        </select>
+      </Stack>
     </div>
   );
 };
