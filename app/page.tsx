21b848d727d1ba62e20b62fa01bf6e3eba1700a4
김동현 <doyn0822@hiconsy.com>
2024-04-29 12:16:12 +0900

    [DS-19]Component - FormsSelect 스타일 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
 
+import Stack from '@components/Stack/Stack';
+
 const Home = () => {
-  return <div className="docs">HomePage</div>;
+  return (
+    <div className="docs">
+      <Stack direction="col">
+        <select name="" id="" className="form__select" disabled>
+          <option value="aaa">aaaa</option>
+          <option value="aaa">aaaa</option>
+        </select>
+        <select name="" id="" className="form__select">
+          <option value="aaa">aaaa</option>
+          <option value="aaa">aaaa</option>
+        </select>
+        <select name="" id="" className="form__select is-valid">
+          <option value="aaa">aaaa</option>
+          <option value="aaa">aaaa</option>
+        </select>
+        <select name="" id="" className="form__select is-invalid">
+          <option value="aaa">aaaa</option>
+          <option value="aaa">aaaa</option>
+        </select>
+      </Stack>
+    </div>
+  );
 };
 
 export default Home;
