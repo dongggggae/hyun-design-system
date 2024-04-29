63c369e1d85abfb98bdd8d6af0048748a0f63f88
김동현 <doyn0822@hiconsy.com>
2024-04-29 12:16:12 +0900

    Select 작업 중

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Home = () => {
-  return <div className="docs">HomePage</div>;
+  return (
+    <div className="docs">
+      <select name="" id="" className="form__select">
+        <option value="aaa">aaaa</option>
+        <option value="aaa">aaaa</option>
+      </select>
+    </div>
+  );
 };
 
 export default Home;
