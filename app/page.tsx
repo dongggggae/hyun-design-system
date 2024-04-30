1da309429e9a6c25e5fd7e125a93465b87a4521d
dongggggae <wjdqls149@naver.com>
2024-04-30 17:51:02 +0900

    HomePage 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,7 @@
-'use client';
 import React from 'react';
-import { useForm } from 'react-hook-form';
-
-import FormsSelect from '@components/Forms/FormsSelect';
 
 const Home = () => {
-  const { register, watch } = useForm();
-  const checkValues = watch('select');
-
-  return (
-    <div className="docs">
-      <FormsSelect id="select" title="폼셀렉트" options={['가나다', '간다', '온다']} {...register('select')} />
-    </div>
-  );
+  return <div className="docs">Home Page</div>;
 };
 
 export default Home;
