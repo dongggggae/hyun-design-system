ba5cc6bfc7b57044c5e76eca94a1e18c1d52df80
김동현 <doyn0822@hiconsy.com>
2024-05-03 11:43:11 +0900

    [DS-99]Docs - FormsCheck, FormsInput 수정

--- app/(route)/components/switch/page.tsx
+++ app/(route)/components/switch/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 import dynamic from 'next/dynamic';
-import React from 'react';
 import { useForm } from 'react-hook-form';
 
 import FormsCheck from '@components/Forms/FormsCheck';
--- app/(route)/components/text-field/page.tsx
+++ app/(route)/components/text-field/page.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 
 import FormsInput from '@components/Forms/FormsInput';
+import Stack from '@components/Stack/Stack';
 import Heading from '@components/Typography/Heading';
 import Text from '@components/Typography/Text';
 
@@ -27,9 +28,11 @@ const TextFiledPage = () => {
           <Heading level="h2" size={2} text="2. Addon" />
           <Text>FormsInput 컴포넌트의 타입에 따라 Addon 컴포넌트를 통해 필드 유형이 추가됩니다.</Text>
           <div className="preview">
-            <FormsInput id="input2" type="text" placeholder="Default TextField" />
-            <FormsInput id="input3" type="password" placeholder="Password TextField" />
-            <FormsInput id="input4" type="search" placeholder="Search TextField" />
+            <Stack gap={4}>
+              <FormsInput id="input2" type="text" placeholder="Default TextField" />
+              <FormsInput id="input3" type="password" placeholder="Password TextField" />
+              <FormsInput id="input4" type="search" placeholder="Search TextField" />
+            </Stack>
           </div>
           <CodeContainer codeString={textFieldCode.addon} />
         </div>
@@ -40,9 +43,11 @@ const TextFiledPage = () => {
             &#39;invalid&#39;는 에러 상태를 나타냅니다.
           </Text>
           <div className="preview">
-            <FormsInput id="input5" type="text" value="Valid TextFiled" />
-            <FormsInput id="input6" type="text" value="Valid TextFiled" state="valid" />
-            <FormsInput id="input7" type="text" value="InValid TextField" state="invalid" />
+            <Stack gap={4}>
+              <FormsInput id="input5" type="text" value="Valid TextFiled" />
+              <FormsInput id="input6" type="text" value="Valid TextFiled" state="valid" />
+              <FormsInput id="input7" type="text" value="InValid TextField" state="invalid" />
+            </Stack>
           </div>
           <CodeContainer codeString={textFieldCode.state} />
         </div>
