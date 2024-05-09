5a4e887c035483513e47496650b08652873ff2a4
김동현 <doyn0822@hiconsy.com>
2024-05-09 15:22:10 +0900

    [DS-17]FormsInput 수정

--- app/_components/hds/Forms/FormsInput.tsx
+++ app/_components/hds/Forms/FormsInput.tsx
@@ -1,9 +1,7 @@
-import React, { useState, InputHTMLAttributes, forwardRef } from 'react';
+import React, { InputHTMLAttributes, forwardRef } from 'react';
 
 import classNames from '@utils/classNames';
 
-import FormsAddon from './FormsAddon';
-import useInput from './FormsContext';
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   id: string;
   type?: 'text' | 'password' | 'search';
@@ -16,34 +14,6 @@ const FormsInput = forwardRef<HTMLInputElement, InputProps>(
   ({ id, type = 'text', state = '', placeholder, disabled, className, ...props }, ref) => {
     const PREFIX = 'form__control';
 
-    const { inputText, onChange } = useInput();
-    const [passwordVisible, setPasswordVisible] = useState(false);
-
-    const togglePasswordVisibility = () => {
-      setPasswordVisible(!passwordVisible);
-    };
-
-    const clearInput = () => {
-      onChange({ target: { value: '' } } as React.ChangeEvent<HTMLInputElement>);
-    };
-
-    const rederAddon = () => {
-      switch (type) {
-        case 'text':
-          return inputText.length > 0 ? <FormsAddon field="clear" onClick={clearInput} /> : null;
-        case 'search':
-          return <FormsAddon field="search" />;
-        case 'password':
-          return inputText.length > 0 ? (
-            <FormsAddon field={passwordVisible ? 'visible' : 'invisible'} onClick={togglePasswordVisibility} />
-          ) : null;
-        default:
-          return null;
-      }
-    };
-
-    const inputType = type === 'password' && passwordVisible ? 'text' : type;
-
     return (
       <div
         className={classNames(
@@ -55,15 +25,12 @@ const FormsInput = forwardRef<HTMLInputElement, InputProps>(
         <input
           id={id}
           ref={ref}
-          type={inputType}
-          value={inputText}
-          onChange={onChange}
+          type={type}
           disabled={disabled}
           placeholder={placeholder}
           className={PREFIX}
           {...props}
         />
-        {rederAddon()}
       </div>
     );
   },
