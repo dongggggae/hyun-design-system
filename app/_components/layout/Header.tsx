f7d35a6358e8663b850b9a708b43d40ec779b18b
dongggggae <wjdqls149@naver.com>
2024-05-14 11:51:01 +0900

    [DS-91]Layout - Home 수정, 현재 경로 표시

--- app/_components/layout/Header.tsx
+++ app/_components/layout/Header.tsx
@@ -1,9 +1,27 @@
+'use client';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 
-import classNames from '@utils/classNames';
+import Heading from '@components/Typography/Heading';
 
 const Header = () => {
-  return <header className={classNames('header')}>Header</header>;
+  const PREFIX = 'header';
+
+  const pathname = usePathname();
+  const formattedPathname = pathname
+    .split('/')
+    .filter((word) => word)
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' / ');
+
+  return (
+    <header className={`${PREFIX}`}>
+      <div className={`${PREFIX}__inner`}>
+        <Heading level="h6" size={6} text={formattedPathname === '' ? 'Home' : formattedPathname} />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
