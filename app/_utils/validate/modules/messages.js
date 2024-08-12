beb994b1f6f21a875c90f1ec4ac5a314e8eeb175
김동현 <doyn0822@hiconsy.com>
2024-08-12 15:31:21 +0900

    [DS-103]Forms - Validation 관련 파일 수정

--- app/_utils/validate/modules/messages.js
+++ app/_utils/validate/modules/messages.js
@@ -1,4 +1,8 @@
 export const messages = {
+  input: (name) => `${name}을(를) 입력해주세요.`,
   password: '영문, 숫자, 특수문자를 포함하여 8자 이상 20자 이하로 입력해주세요.',
   num: '숫자만 입력 가능합니다.',
+  korean: '한글만 입력 가능합니다.',
+  english: '영어만 입력 가능합니다.',
+  confirmPassword: '비밀번호가 일치하지 않습니다.',
 };
--- app/_utils/validate/modules/schema.js
+++ app/_utils/validate/modules/schema.js
@@ -4,18 +4,22 @@ import { messages } from './messages';
 import { regExp } from './regexp';
 
 export const schema = yup.object({
-  username: yup
-    .string()
-    .matches(regExp.name, '이름은 한글 또는 영문만 입력 가능합니다.')
-    .required('이름을 입력해주세요'),
+  input: yup.string().required(messages.input('내용')).matches(regExp.mixed, '공백 없이 입력해주세요.'),
+  name: yup.string().required(messages.input('이름')).matches(regExp.name, '이름은 한글 또는 영문만 입력 가능합니다.'),
+  nameKo: yup.string().required(messages.input('이름')).matches(regExp.korean, messages.korean),
+  nameEng: yup.string().required(messages.input('이름')).matches(regExp.english, messages.english),
   id: yup
     .string()
-    .matches(regExp.id, '아이디는 영문, 숫자, 언더스코어(_), 하이픈(-)만 사용할 수 있습니다.')
-    .required('아이디를 입력해주세요'),
-  email: yup.string().matches(regExp.email, '유효한 이메일 주소를 입력해주세요.').required('이메일을 입력해주세요.'),
-  password: yup.string().matches(regExp.password, messages.password).required('비밀번호를 입력해주세요.'),
+    .required(messages.input('아이디'))
+    .matches(regExp.id, '아이디는 영문, 숫자, 언더스코어(_), 하이픈(-)만 사용할 수 있습니다.'),
+  password: yup.string().required(messages.input('비밀번호')).matches(regExp.password, messages.password),
+  passwordConfirm: yup
+    .string()
+    .required(messages.input('비밀번호'))
+    .oneOf([yup.ref('password'), null], messages.confirmPassword),
+  email: yup.string().required(messages.input('이메일')).matches(regExp.email, '유효한 이메일 주소를 입력해주세요.'),
   phone: yup
     .string()
-    .matches(regExp.phone, '유효한 전화번호 형식을 입력해주세요. (예: 01012345678)')
-    .required('전화번호를 입력해주세요.'),
+    .required(messages.input('전화번호'))
+    .matches(regExp.phone, '유효한 전화번호 형식을 입력해주세요. (예: 01012345678)'),
 });
