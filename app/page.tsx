e5e375d5a7f32460d715570898ee960c59f26e1e
hyun <wjdqls149@naver.com>
2025-01-16 21:05:09 +0900

    컴포넌트 페이지로 리다이렉팅

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
+import { redirect } from 'next/navigation';
 import React from 'react';
 
-const Home = () => {
-  return <div className="docs">HomePage</div>;
-};
-
-export default Home;
+export default function Home() {
+  redirect('/components');
+}
